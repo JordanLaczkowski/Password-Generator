9d6e23a0bd10ff206c3683e426a8fa7ac8e10ce1
Jordan Laczkowski <109110372+JordanLaczkowski@users.noreply.github.com>
2022-08-04 17:56:00 -0600

    Add files via upload
    Change scripts and prompts. Changed where prompts were in the coding

--- script.js
+++ script.js
@@ -1,43 +1,143 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+let upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let specialCharacters = [
+  "?",
+  "!",
+  ">",
+  "<",
+  "{",
+  "}",
+  "[",
+  "]",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "=",
+  "'",
+  ":",
+  ";",
+  ".",
+  ",",
+  "~",
+  "`",
+];
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
 
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-var specialCharacters = []
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-
- // Add event listener to generate button
-generateBtn.addEventListener("click", function myFunction(){writePassword(event, name)});
+// Add event listener to generate button
+generateBtn.addEventListener("click", function myFunction() {
+  writePassword();
+});
 
 function generatePassword() {
-  var length = prompt("How many characters?");
+  var lengthPrompt = prompt("How many characters?");
   console.log(length);
-  var specialCharacters = prompt("Would you like to include special characters?");
-  console.log(specialCharacters);
-  var numeric = prompt("Would you like any numbers?");
+  if (lengthPrompt < 8) {
+    alert("Password needs to have at least 8 characters");
+  } else if (lengthPrompt > 128) {
+    alert("Password needs to be less the 128 characters");
+  }
+  var specialCharactersPrompt = confirm(
+    "Would you like to include special characters?"
+  );
+  console.log(specialCharactersPrompt);
+  if (specialCharactersPrompt) {
+    var specialCharIndex = Math.floor(
+      this.specialCharacters.length * Math.random()
+    );
+  }
+  var numericPrompt = confirm("Would you like any numbers?");
   console.log(numeric);
+  var upperCasePrompt = confirm(
+    "Would you like to include special characters?"
+  );
+  console.log(upperCase);
+  var lowerCasePrompt = confirm("Would you like any numbers?");
+  console.log(lowerCase);
 }
 
+var computerChoice = options[Math.floor(options.length * Math.random())];
 
-
-//From internet 
+// //From internet
 //  function genPassword() {
 //     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 //     var passwordLength = 12;
@@ -52,8 +152,5 @@ function generatePassword() {
 // function copyPassword() {
 //   var copyText = document.getElementById("password");
 //   copyText.select();
-//   document.execCommand("copy");  
+//   document.execCommand("copy");
 // }
-
-
-
