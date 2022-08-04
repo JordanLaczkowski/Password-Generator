d68c30b4737d6dc82bb9d886776e164c3fcd1551
Jordan Laczkowski <109110372+JordanLaczkowski@users.noreply.github.com>
2022-08-04 15:17:05 -0600

    Add files via upload
    added numerics, upper case and lower case letters

--- script.js
+++ script.js
@@ -7,11 +7,53 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialCharacters = []
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+ // Add event listener to generate button
+generateBtn.addEventListener("click", function myFunction(){writePassword(event, name)});
+
+function generatePassword() {
+  var length = prompt("How many characters?");
+  console.log(length);
+  var specialCharacters = prompt("Would you like to include special characters?");
+  console.log(specialCharacters);
+  var numeric = prompt("Would you like any numbers?");
+  console.log(numeric);
+}
+
+
+
+//From internet 
+//  function genPassword() {
+//     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     var passwordLength = 12;
+//     var password = "";
+//  for (var i = 0; i <= passwordLength; i++) {
+//    var randomNumber = Math.floor(Math.random() * chars.length);
+//    password += chars.substring(randomNumber, randomNumber +1);
+//   }
+//         document.getElementById("password").value = password;
+//  }
+
+// function copyPassword() {
+//   var copyText = document.getElementById("password");
+//   copyText.select();
+//   document.execCommand("copy");  
+// }
+
+
+
