49dd1794d74d427c3513ab7fbe56b66e9fcae48f
Jordan <jordan.laczkowski23@gmail.com>
2022-08-05 09:57:08 -0600

    added to my README with title and about the code. Also took out notes from js code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Password Generator Starter Code
+https://jordanlaczkowski.github.io/Password-Generator/
+
+Title: Password Generator
+
+This application is able to give numerous options for new passwords chosing different criterias the user would like. The user must chose "yes" to at least one: Numbers, Special Characters, Upper Case Letter, or Lower Case Letter. The user can chose "yes" to more than one of these if they would like. The user also can choose how many characters they want as long as it is >=8 or <=128. If the needed criteria is used, the password generator will provide a random password.
--- script.js
+++ script.js
@@ -106,7 +106,6 @@ var numericPrompt;
 var count = 0;
 var password = "";
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
 
@@ -115,7 +114,6 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", function myFunction() {
   writePassword();
 });
@@ -206,21 +204,3 @@ function fillRestOfPassword() {
   }
   return password;
 }
-
-// //From internet
-//  function genPassword() {
-//     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     var passwordLength = 12;
-//     var password = "";
-//  for (var i = 0; i <= passwordLength; i++) {
-//    var randomNumber = Math.floor(Math.random() * chars.length);
-//    password += chars.substring(randomNumber, randomNumber +1);
-//   }
-//         document.getElementById("password").value = password;
-//  }
-
-// function copyPassword() {
-//   var copyText = document.getElementById("password");
-//   copyText.select();
-//   document.execCommand("copy");
-// }
